95d0ce558262039b999619c9bdbf667b2b1c4019
nashEm8 <emanuel82022@outlook.com>
2022-12-07 00:56:29 -0300

    Criação da updateClock que vai pegar a data atual(na qual será atualizada a cada 1s por meio do setInterval), depois as horas, minutos e segundos e jogar na clockHTML, onde será exibido na tela por meio do innerHTML da clock-container

---
+++ app.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector('.clock-container')
+
+const updateClock = () => {
+	const present = new Date();
+	const hours = present.getHours();
+	const minutes = present.getMinutes();
+	const seconds = present.getSeconds();
+
+	const clockHTML = `
+	<div class="back-clock">
+		<span>${String(hours).length === 1 ? `0${hours} :` : `${hours} :`}</span>
+		<span>${String(minutes).length === 1 ? `0${minutes} :` : `${minutes} :`}</span>
+		<span>${String(seconds).length === 1 ? `0${seconds}` : `${seconds}`}</span>
+	</div>
+	`;
+
+	clockContainer.innerHTML = clockHTML;
+}
+
+setInterval(updateClock, 1000);
